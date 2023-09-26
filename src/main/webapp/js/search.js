fa7d2fcac8d9b63aa1b346001c550a401893689d
Niihama Atsushi <atsushi_niihama@test.com>
2023-09-26 09:21:54 +0900

    commit

--- src/main/webapp/js/search.js
+++ src/main/webapp/js/search.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
-	$('#community-select').on('change', function() {
-		var selectedCommunityId = $(this).val();
-		window.location.href = "http://localhost:8080/java-training-1.0/control?action_name=topcommunity&communityId="+selectedCommunityId
-    });
-    
     $(".active-item").on("click", function() {
 		const apiId = $(this).attr("id")
 		window.location.href = "http://localhost:8080/java-training-1.0/control?action_name=shop_detail&apiId="+apiId
